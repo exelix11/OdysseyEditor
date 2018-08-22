586edd6f7996f7d1391b070e2eb1b38602555d74
exelix <ex.elix@libero.it>
2018-08-22 18:19:37 +0200

    Support for adding objects

--- EditorForms/AddObjList.Designer.cs
+++ EditorForms/AddObjList.Designer.cs
@@ -28,37 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddObjList));
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(216, 106);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(297, 106);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "OK";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddObjList));
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(216, 106);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "Cancel";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(297, 106);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "OK";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
             "ObjectList",
             "AreaList",
             "SkyList",
@@ -75,34 +75,34 @@
             "CapMessageList",
             "MapIconList",
             "DebugList"});
-            this.comboBox1.Location = new System.Drawing.Point(106, 62);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(170, 21);
-            this.comboBox1.TabIndex = 5;
-            // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(378, 59);
-            this.label1.TabIndex = 4;
-            this.label1.Text = resources.GetString("label1.Text");
-            // 
-            // AddObjList
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(377, 133);
-            this.ControlBox = false;
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "AddObjList";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "AddObjList";
-            this.ResumeLayout(false);
+			this.comboBox1.Location = new System.Drawing.Point(106, 62);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(170, 21);
+			this.comboBox1.TabIndex = 5;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(2, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(378, 59);
+			this.label1.TabIndex = 4;
+			this.label1.Text = resources.GetString("label1.Text");
+			// 
+			// AddObjList
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(377, 133);
+			this.ControlBox = false;
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "AddObjList";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "AddObjList";
+			this.ResumeLayout(false);
 
         }
 
---
+++ EditorForms/AddObjectDialog.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace OdysseyExt.EditorForms
+{
+	partial class AddObjectDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tbFileName = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.tbModelName = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.btnCreate = new System.Windows.Forms.Button();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.tbDescription = new System.Windows.Forms.TextBox();
+			this.chbProp = new System.Windows.Forms.CheckBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.ParConfigName = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// tbFileName
+			// 
+			this.tbFileName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbFileName.Location = new System.Drawing.Point(12, 76);
+			this.tbFileName.Name = "tbFileName";
+			this.tbFileName.Size = new System.Drawing.Size(404, 20);
+			this.tbFileName.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(97, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "ObjectType (name)";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 60);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(256, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Object FileName (not many object types support that)";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 114);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(246, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Object ModelName (not many objects support that)";
+			// 
+			// tbModelName
+			// 
+			this.tbModelName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbModelName.Location = new System.Drawing.Point(12, 130);
+			this.tbModelName.Name = "tbModelName";
+			this.tbModelName.Size = new System.Drawing.Size(404, 20);
+			this.tbModelName.TabIndex = 4;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.comboBox1.DropDownHeight = 250;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.IntegralHeight = false;
+			this.comboBox1.Location = new System.Drawing.Point(15, 26);
+			this.comboBox1.MaxDropDownItems = 100;
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(401, 21);
+			this.comboBox1.Sorted = true;
+			this.comboBox1.TabIndex = 6;
+			this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextUpdate);
+			// 
+			// btnCreate
+			// 
+			this.btnCreate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnCreate.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.btnCreate.Enabled = false;
+			this.btnCreate.Location = new System.Drawing.Point(330, 364);
+			this.btnCreate.Name = "btnCreate";
+			this.btnCreate.Size = new System.Drawing.Size(86, 23);
+			this.btnCreate.TabIndex = 7;
+			this.btnCreate.Text = "Ok";
+			this.btnCreate.UseVisualStyleBackColor = true;
+			this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btnCancel.Location = new System.Drawing.Point(238, 364);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(86, 23);
+			this.btnCancel.TabIndex = 8;
+			this.btnCancel.Text = "Cancel";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+			// 
+			// tbDescription
+			// 
+			this.tbDescription.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbDescription.Location = new System.Drawing.Point(12, 209);
+			this.tbDescription.Multiline = true;
+			this.tbDescription.Name = "tbDescription";
+			this.tbDescription.ReadOnly = true;
+			this.tbDescription.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.tbDescription.Size = new System.Drawing.Size(404, 149);
+			this.tbDescription.TabIndex = 9;
+			this.tbDescription.Text = "No Desription Provided";
+			// 
+			// chbProp
+			// 
+			this.chbProp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.chbProp.AutoSize = true;
+			this.chbProp.Location = new System.Drawing.Point(12, 368);
+			this.chbProp.Name = "chbProp";
+			this.chbProp.Size = new System.Drawing.Size(192, 17);
+			this.chbProp.TabIndex = 10;
+			this.chbProp.Text = "Don\'t add object-specific properties";
+			this.chbProp.UseVisualStyleBackColor = true;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 166);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(295, 13);
+			this.label4.TabIndex = 12;
+			this.label4.Text = "Object ParameterConfigName (not many objects support that)";
+			// 
+			// ParConfigName
+			// 
+			this.ParConfigName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.ParConfigName.FormattingEnabled = true;
+			this.ParConfigName.Items.AddRange(new object[] {
+            "FixMapParts"});
+			this.ParConfigName.Location = new System.Drawing.Point(12, 182);
+			this.ParConfigName.Name = "ParConfigName";
+			this.ParConfigName.Size = new System.Drawing.Size(404, 21);
+			this.ParConfigName.TabIndex = 13;
+			// 
+			// AddObjectDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(428, 399);
+			this.ControlBox = false;
+			this.Controls.Add(this.ParConfigName);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.chbProp);
+			this.Controls.Add(this.tbDescription);
+			this.Controls.Add(this.btnCancel);
+			this.Controls.Add(this.btnCreate);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.tbModelName);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.tbFileName);
+			this.Name = "AddObjectDialog";
+			this.Text = "Add Object";
+			this.Load += new System.EventHandler(this.comboBox1_TextUpdate);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TextBox tbFileName;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox tbModelName;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button btnCreate;
+		private System.Windows.Forms.Button btnCancel;
+		private System.Windows.Forms.TextBox tbDescription;
+		private System.Windows.Forms.CheckBox chbProp;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ComboBox ParConfigName;
+	}
+}
