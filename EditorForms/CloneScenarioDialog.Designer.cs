3d5afd3b2bc695b9cbf1acb0f11a40c2063f5e62
exelix <ex.elix@libero.it>
2018-11-21 19:54:46 +0100

    Clone scenario button

---
+++ EditorForms/CloneScenarioDialog.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace OdysseyExt.EditorForms
+{
+	partial class CloneScenarioDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CloneScenarioDialog));
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.CurScenarioLbl = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Items.AddRange(new object[] {
+            "Scenario 0",
+            "Scenario 1"});
+			this.checkedListBox1.Location = new System.Drawing.Point(4, 61);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(266, 229);
+			this.checkedListBox1.TabIndex = 0;
+			// 
+			// CurScenarioLbl
+			// 
+			this.CurScenarioLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.CurScenarioLbl.AutoSize = true;
+			this.CurScenarioLbl.Location = new System.Drawing.Point(2, 299);
+			this.CurScenarioLbl.Name = "CurScenarioLbl";
+			this.CurScenarioLbl.Size = new System.Drawing.Size(124, 13);
+			this.CurScenarioLbl.TabIndex = 1;
+			this.CurScenarioLbl.Text = "The current scenario is : ";
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.Location = new System.Drawing.Point(2, 5);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(269, 53);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Select the scenarios you want to replace with the current one.\r\nWARNING: This wil" +
+    "l replace the selected scenarios, all their data will be LOST.";
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Location = new System.Drawing.Point(210, 392);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(61, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "OK";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.button2.Location = new System.Drawing.Point(143, 392);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(61, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Cancel";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.checkBox1.Location = new System.Drawing.Point(4, 315);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(266, 71);
+			this.checkBox1.TabIndex = 5;
+			this.checkBox1.Text = resources.GetString("checkBox1.Text");
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// CloneScenarioDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(274, 416);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.CurScenarioLbl);
+			this.Controls.Add(this.checkedListBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "CloneScenarioDialog";
+			this.Text = "Clone scenario";
+			this.TopMost = true;
+			this.Load += new System.EventHandler(this.CloneScenarioDialog_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.CheckedListBox checkedListBox1;
+		private System.Windows.Forms.Label CurScenarioLbl;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.CheckBox checkBox1;
+	}
+}
---
+++ EditorForms/CloneScenarioDialog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OdysseyExt.EditorForms
+{
+	public partial class CloneScenarioDialog : Form
+	{
+		public int[] result = null;
+		public bool clone = true;
+
+		class ScenarioString
+		{
+			public ScenarioString(int i) { Tag = i; Str = "Scenario " + i; }
+			public string Str;
+			public int Tag;
+			public override string ToString() => Str;
+		}
+
+		public CloneScenarioDialog(int curScenario, int ScenarioCount)
+		{
+			InitializeComponent();
+			CurScenarioLbl.Text += curScenario.ToString();
+			checkedListBox1.Items.Clear();
+			for (int i = 0; i < ScenarioCount; i++)
+				if (curScenario != i)
+					checkedListBox1.Items.Add(new ScenarioString(i));
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (MessageBox.Show("Are you sure ?", "", MessageBoxButtons.YesNo) != DialogResult.Yes)
+				return;
+
+			result = new int[checkedListBox1.CheckedItems.Count];
+			int i = 0;
+			foreach (ScenarioString item in checkedListBox1.CheckedItems)
+				result[i++] = item.Tag;
+			clone = !checkBox1.Checked;
+			this.Close();
+		}
+
+		private void CloneScenarioDialog_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			result = null;
+			this.Close();
+		}
+	}
+}
